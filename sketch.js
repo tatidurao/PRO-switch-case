dd86f1e65bb63da425362fdda286b0ea7c1e0232
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2024-07-19 14:24:45 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,17 @@ function setup()
 {
   createCanvas(400, 400);
   background(178,255,102);
+  
+  //criar caixa de texto
+  input = createInput();
+  //posicionar
+  input.position(5,50);
+  
+  heading = createElement('h4', 'Insira qualquer letra do Alfabeto');
+  heading.position(5,1);
+
+ textAlign(CENTER);
+ textSize(50);
  
 }
 
